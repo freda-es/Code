bba0d85179a007abaf6c98300c53ba87cbf319f1
Freda Dyrkaj <86465451+freda-es@users.noreply.github.com>
2022-07-08 12:07:17 +0200

    Update sendsms.py

--- sendsms.py
+++ sendsms.py
@@ -16,7 +16,7 @@ def connect_mongodb (db_username, db_passw, db_name):
     return database
 
 #call the function to generate your own db
-databases = connect_mongodb (db_username='freda', db_passw='19freda87', db_name='mydb')
+databases = connect_mongodb (db_username='', db_passw='', db_name='mydb')
 #generate my clusters
 my_cluster = databases.mobile_numbers
 
@@ -34,22 +34,22 @@ my_number = '+355' + str(list_numbers[0])
 import os
 from twilio.rest import Client
  
-account_sid = 'ACf154a88c2e2b9e4ac792fc4a6834227c' 
-auth_token = '3e23e49549cca47624827349f622cc9d' 
+account_sid = 'account_sidc' 
+auth_token = 'auth_token' 
 client1 = Client(account_sid, auth_token) 
  
 #sending sms
 message = client1.messages.create(  
-                              messaging_service_sid='MG3a02647da11bba2f4ef4d9b179f3bb11', 
+                              messaging_service_sid='MG3a02647d....', 
                               body='HelloFreda',      
-                              to='+355693989000'
+                              to='+3556939.....'
                           ) 
 
 #sending whatsup sms
 # message = client1.messages.create(
 #                               body='Hello there!',
-#                               from_='whatsapp:+14155238886',
-#                               to='whatsapp:+355693989000'
+#                               from_='whatsapp:+14155....',
+#                               to='whatsapp:+3556939.....'
 #                           )
  
 print(message.sid)
